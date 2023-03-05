7af925cc0b201ace76592c5a1596bbea0f6d1e11
wprestonneal <mrprestonneal@gmail.com>
2023-03-04 17:17:05 -0800

    arrays

--- chapter_3/src/main.rs
+++ chapter_3/src/main.rs
@@ -1,13 +1,8 @@
 fn main() {
-    let tup: (i32, bool, f32) = (-16, true, 17.8);
+    // arrays have fixed size
+    // declare size in type declaration
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
 
-    // can destructure a tuple
-    let (x, y, z) = tup;
-
-    println!("the value of y is {y}");
-
-    // or index with .
-    let negative_sixteen = tup.0;
-
-    println!("the value of tup.0 is {negative_sixteen}");
+    // fill an array
+    let b = [27; 5]
 }
