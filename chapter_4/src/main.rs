601be03e7c26555bf0a1eb4b3cfcb8839ea67895
wprestonneal <mrprestonneal@gmail.com>
2023-03-05 11:10:58 -0800

    more ownership vs copy

--- chapter_4/src/main.rs
+++ chapter_4/src/main.rs
@@ -1,8 +1,20 @@
 fn main() {
     let s1 = String::from("hello world");
-    let s2 = s1;
     
-    println!("{}", s1);
+    takes_ownership(s1);
+
+    println!("{}", s1); // this wont work
+
+
+    let x = 5;
+    makes_copy(x);
+    println!("{}", x); // but this still does
 }
 
+fn takes_ownership(some_string: String) {
+    println!("{}", some_string);
+}
 
+fn makes_copy(some_integer: i32) {
+    println!("{}", some_integer);
+}
