13dff4f967bca06c6634b35752dff39d9f2d0039
wprestonneal <mrprestonneal@gmail.com>
2023-03-05 11:57:09 -0800

    compiles but not good solution

--- chapter_4/src/main.rs
+++ chapter_4/src/main.rs
@@ -1,8 +1,17 @@
 fn main() {    
-    let reference_to_nothing = dangle();
+    let mut s = String::from("hello world");
+    let word = first_word(&s);
+    s.clear();
 }
 
-fn dangle() -> &String {
-    let s = String::from("hello");
-    &s
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i
+        }
+    }
+
+    s.len()
 }
