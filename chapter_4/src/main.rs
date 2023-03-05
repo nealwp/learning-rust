fac36219cf0f58f8ec4404eb066f837a8adf9f88
wprestonneal <mrprestonneal@gmail.com>
2023-03-05 11:52:04 -0800

    cant dangle a reference

--- chapter_4/src/main.rs
+++ chapter_4/src/main.rs
@@ -1,10 +1,8 @@
-fn main() {
-    
-    let mut some_string = String::from("yours");
-    
-    let r1 = &some_string;
-    let r2 = &some_string;
-    let r3 = &mut some_string;
+fn main() {    
+    let reference_to_nothing = dangle();
+}
 
-    println!("{}, {}, {}", r1, r2, r3);
+fn dangle() -> &String {
+    let s = String::from("hello");
+    &s
 }
