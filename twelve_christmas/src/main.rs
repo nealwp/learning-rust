84d3a4df5b29dd0daa2a730692aff2d17e0d53d5
wprestonneal <mrprestonneal@gmail.com>
2023-03-04 18:14:00 -0800

    add days and gifts

---
+++ twelve_christmas/src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    let days = ["first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth"];
+    
+    let gifts = [
+        "a patridge in a pear tree",
+        "2 turtle doves",
+        "3 french hens",
+        "4 calling birds",
+        "5 golden rings",
+        "6 geese a laying",
+        "7 swans a swimming",
+        "8 maids a milking",
+        "9 ladies dancing",
+        "10 lords a leaping",
+        "11 pipers piping",
+        "12 drummers drumming"
+    ];
+
+    for day in days {
+        println!("On the {day} day of christmas my true love gave to me");
+    }
+}
