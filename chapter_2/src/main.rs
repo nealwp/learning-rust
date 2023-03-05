7234dcfa291cc0230e52c2a28e5661d658493934
wprestonneal <mrprestonneal@gmail.com>
2023-03-04 16:55:00 -0800

    shadowing and inner scope

--- chapter_2/src/main.rs
+++ chapter_2/src/main.rs
@@ -1,6 +1,13 @@
+const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
 fn main() {
-    let mut x = 5;
-    println!("the value of x is {x}");
-    x = 6;
+    let x = 5;
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in inner scope is {x}");
+    }
+
     println!("the value of x is {x}");
 }
