b8c40179235f76d9a51c08b98f9046d5fd2e2c28
wprestonneal <mrprestonneal@gmail.com>
2023-03-05 11:29:10 -0800

    return a tuple for multivalue return

--- chapter_4/src/main.rs
+++ chapter_4/src/main.rs
@@ -1,19 +1,14 @@
 fn main() {
-    let s1 = gives_ownership();
-    println!("s1: {}", s1); // ownership from function
     
-    let s2 = String::from("hi");
-    let s3 = takes_and_gives_back(s2);
-
-    println!("s3: {}", s3); // ownership passed from main to fn and back
-
-}
-
-fn gives_ownership() -> String {
     let some_string = String::from("yours");
-    some_string
+    
+    let (s2, len) = calculate_length(some_string);
+
+    println!("the length of {} is {}", s2, len); 
+
 }
 
-fn takes_and_gives_back(a_string: String) -> String {
-    a_string
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len();
+    (s, length)
 }
