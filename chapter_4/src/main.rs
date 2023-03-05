d143bd049fd62b423f37330f8e93bac36e732639
wprestonneal <mrprestonneal@gmail.com>
2023-03-05 11:48:55 -0800

    cannot have both mutable and immutable borrows at once

--- chapter_4/src/main.rs
+++ chapter_4/src/main.rs
@@ -2,8 +2,9 @@ fn main() {
     
     let mut some_string = String::from("yours");
     
-    let r1 = &mut some_string;
-    let r2 = &mut some_string;
+    let r1 = &some_string;
+    let r2 = &some_string;
+    let r3 = &mut some_string;
 
-    println!("{}, {}", r1, r2);
+    println!("{}, {}, {}", r1, r2, r3);
 }
