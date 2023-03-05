254f78152164e235797958b46be3d606afbe1598
wprestonneal <mrprestonneal@gmail.com>
2023-03-05 12:04:14 -0800

    using string slice

--- chapter_4/src/main.rs
+++ chapter_4/src/main.rs
@@ -1,11 +1,10 @@
 fn main() {    
-    let mut s = String::from("hello world");
+    let s = String::from("hello world");
     let word = first_word(&s);
-    s.clear();
     println!("the first word is: {}", word);
 }
 
-fn first_word(s: &String) -> &str {
+fn first_word(s: &str) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
